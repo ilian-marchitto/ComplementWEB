29cdad80f6dd608fe1701f90fe8d383eca42829d
ilian-marchitto <ilian.marchitto@etu.univ-amu.fr>
2026-01-22 12:14:27 +0100

    ajout de commentaires

--- script.js
+++ script.js
@@ -31,26 +31,39 @@ function afficherValeurParValeur() {
             let O_temperatureMeter = document.getElementById("temperatureMeter");
             O_commentaire.hidden = true;
             if (A_tableau20Valeurs[i] < 0) {
+
                 O_commentaire.hidden = false;
                 O_commentaire.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
+
                 O_elementDiv.className = "bordure-bleue";
+
                 O_temperatureMeter.setAttribute("value", A_tableau20Valeurs[i]);
+
             } else if (A_tableau20Valeurs[i] >= 0 && A_tableau20Valeurs[i] < 20) {
+
                 O_commentaire.hidden = true;
                 O_elementDiv.className = "bordure-verte";
+
                 O_temperatureMeter.setAttribute("value", A_tableau20Valeurs[i]);
+
             } else if (A_tableau20Valeurs[i] >= 20 && A_tableau20Valeurs[i] < 30) {
+
                 O_commentaire.hidden = true;
                 O_elementDiv.className = "bordure-orange";
+
                 O_temperatureMeter.setAttribute("value", A_tableau20Valeurs[i]);
+
             } else if (A_tableau20Valeurs[i] >= 30) {
+
                 O_commentaire.hidden = false;
                 O_commentaire.textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
+
                 O_elementDiv.className = "bordure-rouge";
+
                 O_temperatureMeter.setAttribute("value", A_tableau20Valeurs[i]);
             }
             
-        } else {i = -1;}
+        } else { i = -1; } // Pour que ça le fasse en boucle 
         clearInterval();
         i++;
     }, 2000);
